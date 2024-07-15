cb9b40b9a5830a052c729c087d59bc8149e23168
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-07-15 11:59:59 +0000

    test: add category entity tests

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -31,4 +31,62 @@ describe("Category unit tests", () => {
     expect(category.is_active).toBeFalsy();
     expect(category.created_at).toBeInstanceOf(Date);
   });
+
+  test("should change category name", () => {
+    const category = new Category({
+      name: "Movie",
+    });
+
+    category.changeName("New Movie");
+    expect(category.name).toBe("New Movie");
+  });
+
+  it("should change category description", () => {
+    const category = new Category({
+      name: "Movie",
+    });
+
+    category.changeDescription("New description");
+    expect(category.description).toBe("New description");
+  });
+});
+
+describe("create command", () => {
+  test("shoud create a category", () => {
+    const category = Category.create({
+      name: "Movie",
+    });
+
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe("Movie");
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+
+  test("should create a category with description", () => {
+    const category = Category.create({
+      name: "Movie",
+      description: "Movie description",
+    });
+
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe("Movie");
+    expect(category.description).toBe("Movie description");
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+
+  test("should create a category with is_active = false", () => {
+    const category = Category.create({
+      name: "Movie",
+      is_active: false,
+    });
+
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe("Movie");
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeFalsy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
 });
