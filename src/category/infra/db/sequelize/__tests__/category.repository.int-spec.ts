2f685ee6712cd150f2f3f9fe3de079ff05e611a0
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-01 11:51:40 -0300

    test: improve category repository integration tests

--- src/category/infra/db/sequelize/__tests__/category.repository.int-spec.ts
+++ src/category/infra/db/sequelize/__tests__/category.repository.int-spec.ts
@@ -4,6 +4,7 @@ import { CategoryRepository } from '../category.repository';
 import { Category } from '../../../../domain/category.entity';
 import { NotFoundError } from '../../../../../shared/domain/errors/not-found.error';
 import { CategorySearchParams } from '../../../../domain/category.repository.interface';
+import { Uuid } from '../../../../../shared/domain/value-objects/uuid.vo';
 
 describe('CategoryRepository integration tests', () => {
   let sequelize: Sequelize;
@@ -82,6 +83,34 @@ describe('CategoryRepository integration tests', () => {
     await expect(repository.delete(category.category_id)).rejects.toThrow(NotFoundError);
   });
 
+  it('should find a category by id', async () => {
+    const category = Category.fake().aCategory().build();
+
+    await repository.insert(category);
+
+    const foundCategory = await repository.findById(category.category_id);
+
+    expect(foundCategory).toStrictEqual(category);
+  });
+
+  it('should return null when trying to find by no-existing id', async () => {
+    const category = await repository.findById(new Uuid());
+    expect(category).toBeNull();
+  });
+
+  it('should return all categories', async () => {
+    const category1 = Category.fake().aCategory().build();
+    const category2 = Category.fake().aCategory().build();
+    const category3 = Category.fake().aCategory().build();
+
+    await repository.bulkInsert([category1, category2, category3]);
+
+    const categories = await repository.findAll();
+
+    expect(categories).toHaveLength(3);
+    expect(JSON.stringify(categories)).toEqual(JSON.stringify([category1, category2, category3]));
+  });
+
   it('should search categories', async () => {
     const category1 = Category.fake().aCategory().withName('movie').build();
     const category2 = Category.fake().aCategory().withName('serie').build();
@@ -92,12 +121,78 @@ describe('CategoryRepository integration tests', () => {
     const searchResult = await repository.search(new CategorySearchParams({ filter: 'movie' }));
 
     expect(searchResult.items).toHaveLength(1);
-    expect(searchResult.toJSON({ forceEntity: true })).toMatchObject({
+    expect(searchResult.toJSON({ forceEntity: true })).toStrictEqual({
       items: [category1.toJSON()],
       total: 1,
       current_page: 1,
       per_page: 15,
       last_page: 1,
+    });
+  });
+
+  it('should search categories with pagination and sort', async () => {
+    const category1 = Category.fake().aCategory().withName('movie').build();
+    const category2 = Category.fake().aCategory().withName('serie').build();
+    const category3 = Category.fake().aCategory().withName('anime').build();
+
+    await repository.bulkInsert([category1, category2, category3]);
+
+    const searchResult = await repository.search(
+      new CategorySearchParams({ per_page: 1, sort: 'name', sort_direction: 'asc' }),
+    );
+
+    expect(searchResult.items).toHaveLength(1);
+    expect(searchResult.toJSON({ forceEntity: true })).toStrictEqual({
+      items: [category3.toJSON()],
+      total: 3,
+      current_page: 1,
+      per_page: 1,
+      last_page: 3,
+    });
+  });
+
+  it('should search categories with filter, pagination and sort', async () => {
+    const category1 = Category.fake().aCategory().withName('movie').build();
+    const category2 = Category.fake().aCategory().withName('serie').build();
+    const category3 = Category.fake().aCategory().withName('anime').build();
+
+    await repository.bulkInsert([category1, category2, category3]);
+
+    const searchResult = await repository.search(
+      new CategorySearchParams({
+        filter: 'movie',
+        per_page: 1,
+        sort: 'name',
+        sort_direction: 'asc',
+      }),
+    );
+
+    expect(searchResult.items).toHaveLength(1);
+    expect(searchResult.toJSON({ forceEntity: true })).toStrictEqual({
+      items: [category1.toJSON()],
+      total: 1,
+      current_page: 1,
+      per_page: 1,
+      last_page: 1,
+    });
+  });
+
+  it('should search categories with pagination', async () => {
+    const category1 = Category.fake().aCategory().withName('movie').withCreatedAt(new Date('2024-12-03')).build();
+    const category2 = Category.fake().aCategory().withName('serie').withCreatedAt(new Date('2024-12-01')).build();
+    const category3 = Category.fake().aCategory().withName('anime').withCreatedAt(new Date('2024-12-02')).build();
+
+    await repository.bulkInsert([category1, category2, category3]);
+
+    const searchResult = await repository.search(new CategorySearchParams({ per_page: 2 }));
+
+    expect(searchResult.items).toHaveLength(2);
+    expect(searchResult.toJSON({ forceEntity: true })).toStrictEqual({
+      items: [category1.toJSON(), category3.toJSON()],
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
     });
   });
 });
