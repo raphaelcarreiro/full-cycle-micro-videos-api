6dd0d4826c3cf922cc64ac95ac2d3c197f729b3b
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-01 15:20:33 +0000

    test: add category model integration tests

---
+++ src/category/infra/db/sequelize/__tests__/category-model-mapper.int-spec.ts
@@ -0,0 +1,48 @@
+import { Sequelize } from 'sequelize-typescript';
+import { CategoryModel } from '../category.model';
+import { CategoryModelMapper } from '../category-model-mapper';
+import { Uuid } from '../../../../../shared/domain/value-objects/uuid.vo';
+import { Category } from '../../../../domain/category.entity';
+
+describe('CategoryModelMapper tests', () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      models: [CategoryModel],
+      logging: false,
+    });
+
+    await sequelize.sync({ force: true });
+  });
+
+  it('should map from model to entity', () => {
+    const model = CategoryModel.build({
+      category_id: new Uuid().value,
+      name: 'name',
+      description: 'description',
+      created_at: new Date(),
+      is_active: true,
+    });
+
+    const entity = CategoryModelMapper.toEntity(model);
+
+    expect(entity.category_id.value).toBe(model.category_id);
+    expect(entity.name).toBe(model.name);
+    expect(entity.is_active).toBe(model.is_active);
+    expect(entity.description).toBe(model.description);
+  });
+
+  it('should map from entity to model', async () => {
+    const entity = Category.fake().aCategory().build();
+
+    const model = CategoryModelMapper.toModel(entity);
+
+    expect(model.category_id).toBe(entity.category_id.value);
+    expect(model.name).toBe(entity.name);
+    expect(model.is_active).toBe(entity.is_active);
+    expect(model.description).toBe(entity.description);
+  });
+});
