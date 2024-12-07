0064890e4ce484d9609e73a293a11f67434fd8cd
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-07 18:36:04 +0000

    feat: add delete category use case

---
+++ src/category/application/delete-category.use-case.ts
@@ -0,0 +1,31 @@
+import { IUseCase } from '../../shared/application/use-case-interface';
+import { NotFoundError } from '../../shared/domain/errors/not-found.error';
+import { Uuid } from '../../shared/domain/value-objects/uuid.vo';
+import { Category } from '../domain/category.entity';
+import { ICategoryRepository } from '../domain/category.repository.interface';
+
+type Input = {
+  id: string;
+};
+
+type Output = void;
+
+export class DeleteCategoryUseCase implements IUseCase<Input, Output> {
+  constructor(private readonly repository: ICategoryRepository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const category = await this.find(input.id);
+
+    await this.repository.delete(category.category_id);
+  }
+
+  private async find(id: string) {
+    const category = await this.repository.findById(new Uuid(id));
+
+    if (!category) {
+      throw new NotFoundError(id, Category);
+    }
+
+    return category;
+  }
+}
