207624741c3c9ed82124c16eccf7b122af2ec52a
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-11 08:45:10 -0300

    test: improve value object tests

--- src/shared/domain/__tests__/value-object.spec.ts
+++ src/shared/domain/__tests__/value-object.spec.ts
@@ -1,4 +1,4 @@
-import { ValueObject } from "../value-object";
+import { ValueObject } from '../value-object';
 
 class StringValueObject extends ValueObject {
   constructor(public readonly value: string) {
@@ -12,22 +12,37 @@ class ComplexValueObject extends ValueObject {
   }
 }
 
-describe("value object unit tests", () => {
-  it("should compare and get falsy", () => {
-    const first = new StringValueObject("Raphael");
-    const second = new StringValueObject("Carreiro");
-
-    first.equals(second);
+describe('value object unit tests', () => {
+  it('should compare and get falsy', () => {
+    const first = new StringValueObject('Raphael');
+    const second = new StringValueObject('Carreiro');
 
     expect(first.equals(second)).toBeFalsy();
   });
 
-  it("should compare and get truthy", () => {
+  it('should compare and get truthy', () => {
     const first = new ComplexValueObject(1, 1);
     const second = new ComplexValueObject(1, 1);
 
-    first.equals(second);
-
     expect(first.equals(second)).toBeTruthy();
+  });
+
+  it('should compare and get false when equals method received null as parameter value', () => {
+    const first = new StringValueObject('Raphael');
+
+    expect(first.equals(null)).toBeFalsy();
+  });
+
+  it('should compare and get false when equals method received undefined as parameter value', () => {
+    const first = new StringValueObject('Raphael');
+
+    expect(first.equals(undefined)).toBeFalsy();
+  });
+
+  it('should compare and get false when vo are differents', () => {
+    const first = new StringValueObject('Raphael');
+    const second = new ComplexValueObject(1, 1);
+
+    expect(first.equals(second as any)).toBeFalsy();
   });
 });
