d66fe558b00e1f977c88331d41eb035fc7ecb39b
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-13 09:45:10 -0300

    feat: add create category  input

---
+++ src/category/application/use-cases/create/create-category.input.ts
@@ -0,0 +1,33 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, validateSync } from 'class-validator';
+
+type Payload = {
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+};
+
+export class CreateCategoryInput {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description: string | null;
+
+  @IsBoolean()
+  @IsOptional()
+  is_active: boolean;
+
+  constructor(payload: Payload) {
+    this.name = payload.name;
+    this.description = payload.description;
+    this.is_active = payload.is_active;
+  }
+}
+
+export class ValidateCreateCategoryInput {
+  static validate(payload: Payload) {
+    validateSync(new CreateCategoryInput(payload));
+  }
+}
