24e92119e4a168022817c37000ec9ab3abb41cb0
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-13 08:41:28 -0300

    fix: category validator

--- src/category/domain/category.validator.ts
+++ src/category/domain/category.validator.ts
@@ -2,7 +2,6 @@ import { IsNumber, IsString, Max, MaxLength } from 'class-validator';
 import { Category } from './category.entity';
 import { ClassValidatorFields } from '../../shared/domain/validators/class-validator-fields';
 import { Notification } from '../../shared/validators/notification';
-import { plainToClass, plainToInstance } from 'class-transformer';
 
 export class CategoryRules {
   @MaxLength(255, {
@@ -11,13 +10,13 @@ export class CategoryRules {
   name: string;
 
   constructor(category: Category) {
-    Object.assign(this, plainToInstance(CategoryRules, category));
+    Object.assign(this, category);
   }
 }
 
 export class CategoryValidator extends ClassValidatorFields {
   validate(notification: Notification, data: any, fields?: string[]): boolean {
-    const _fields = fields.length ? fields : ['name'];
+    const _fields = fields?.length ? fields : ['name'];
     return super.validate(notification, new CategoryRules(data), _fields);
   }
 }
