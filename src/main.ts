9e531573f462c8f8bb211593032807cecdb5fab6
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-15 16:25:27 +0000

    feat: log on bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(port);
+
+  Logger.log(`Nest applition running on port:${port}`, 'Bootstrap');
 }
 bootstrap();
