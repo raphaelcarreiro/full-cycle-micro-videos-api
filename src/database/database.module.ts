472c1c3c4ed00820b3e852980f67f0a8281efb03
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-19 12:33:43 +0000

    feat: add config module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,14 +1,41 @@
-import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { DB_SCHEMA_TYPE } from 'src/config/config.module';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      host: ':memory:',
-      logging: false,
-      models: [CategoryModel],
+    SequelizeModule.forRootAsync({
+      useFactory: (config: ConfigService<DB_SCHEMA_TYPE>) => {
+        const dbVendor = config.get('DB_VENDOR');
+
+        if (dbVendor === 'sqlite') {
+          return {
+            dialect: 'sqlite',
+            storage: config.get('DB_DATABASE'),
+            logging: config.get('DB_LOGGING'),
+            autoLoadModels: config.get('DB_AUTO_LOAD_MODELS'),
+            synchronize: true,
+          };
+        }
+
+        if (dbVendor === 'mysql') {
+          return {
+            dialect: 'mysql',
+            host: config.get('DB_HOST'),
+            port: config.get('DB_PORT'),
+            username: config.get('DB_USERNAME'),
+            password: config.get('DB_PASSWORD'),
+            database: config.get('DB_DATABASE'),
+            logging: config.get('DB_LOGGING'),
+            autoLoadModels: config.get('DB_AUTO_LOAD_MODELS'),
+            synchronize: true,
+          };
+        }
+
+        throw new Error(`Unsupported database vendor: ${dbVendor}`);
+      },
+      inject: [ConfigService],
     }),
   ],
 })
