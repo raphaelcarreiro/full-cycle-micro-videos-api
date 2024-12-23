b359a3a682175bbd3f8f3cf3b530bf954c8bf5f2
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-23 17:12:33 +0000

    feat: add categories providers

---
+++ src/nest-modules/categories/categories-providers.ts
@@ -0,0 +1,58 @@
+import { CreateCategoryUseCase } from '@core/category/application/use-cases/create/create-category.use-case';
+import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete/delete-category.use-case';
+import { GetCategoriesUseCase } from '@core/category/application/use-cases/list/get-categories.use-case';
+import { GetCategoryUseCase } from '@core/category/application/use-cases/show/get-category.use-case';
+import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update/update-category.use-case';
+import { CategoryInMemoryRepository } from '@core/category/infra/db/in-memory/category-in-memory.repository';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { CategoryRepository } from '@core/category/infra/db/sequelize/category.repository';
+import { getModelToken } from '@nestjs/sequelize';
+
+export const REPOSITORIES = {
+  CATEGORY_REPOSITORY: {
+    provide: CategoryRepository,
+    useExisting: CategoryRepository,
+  },
+  CATEGORY_IN_MEMORY_REPOSITORY: {
+    provide: CategoryInMemoryRepository,
+    useClass: CategoryInMemoryRepository,
+  },
+  CATEGORY_SEQUELIZE_REPOSITORY: {
+    provide: CategoryRepository,
+    useFactory: model => new CategoryRepository(model),
+    inject: [getModelToken(CategoryModel)],
+  },
+};
+
+export const USE_CASES = {
+  CREATE_CATEGORY_USE_CASE: {
+    provide: CreateCategoryUseCase,
+    useFactory: repository => new CreateCategoryUseCase(repository),
+    inject: [REPOSITORIES.CATEGORY_REPOSITORY.provide],
+  },
+  UPDATE_CATEGORY_USE_CASE: {
+    provide: UpdateCategoryUseCase,
+    useFactory: repository => new UpdateCategoryUseCase(repository),
+    inject: [REPOSITORIES.CATEGORY_REPOSITORY.provide],
+  },
+  DELETE_CATEGORY_USE_CASE: {
+    provide: DeleteCategoryUseCase,
+    useFactory: repository => new DeleteCategoryUseCase(repository),
+    inject: [REPOSITORIES.CATEGORY_REPOSITORY.provide],
+  },
+  GET_CATEGORIES_USE_CASE: {
+    provide: GetCategoriesUseCase,
+    useFactory: repository => new GetCategoriesUseCase(repository),
+    inject: [REPOSITORIES.CATEGORY_REPOSITORY.provide],
+  },
+  GET_CATEGORY_USE_CASE: {
+    provide: GetCategoryUseCase,
+    useFactory: repository => new GetCategoryUseCase(repository),
+    inject: [REPOSITORIES.CATEGORY_REPOSITORY.provide],
+  },
+};
+
+export const CATEGORY_PROVIDERS = {
+  REPOSITORIES,
+  USE_CASES,
+};
