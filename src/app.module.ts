40ee52ad447ef3085612f110c32a8e045075b566
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2024-12-28 18:31:55 +0000

    test: add collection and pagination presenter tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './nest-modules/categories/categories.module';
 import { ConfigModule } from './nest-modules/config/config.module';
 import { DatabaseModule } from './nest-modules/database/database.module';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './nest-modules/shared/shared.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), CategoriesModule, DatabaseModule, SharedModule],
