f25b8ac5345987671ac5e28dc98633cbdc274ff8
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-01-04 15:04:43 +0000

    feat: add create table categories migration

---
+++ src/core/shared/infra/db/sequelize/migrations/2025.01.04T14.52.21.create-categories-table.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import type { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable('categories', {
+    category_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE(3),
+      allowNull: false,
+    },
+  });
+};
+export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable('categories');
+};
