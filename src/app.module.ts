9c66deaf05ca2e57ac08a221f6efa6a5b5f8e327
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-01-24 14:42:53 +0000

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { CategoriesModule } from './nest-modules/categories/categories.module';
 import { ConfigModule } from './nest-modules/config/config.module';
 import { DatabaseModule } from './nest-modules/database/database.module';
 import { SharedModule } from './nest-modules/shared/shared.module';
+import { CastMemberModule } from './nest-modules/cast-member/cast-member.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CategoriesModule, DatabaseModule, SharedModule],
+  imports: [ConfigModule.forRoot(), CategoriesModule, CastMemberModule, DatabaseModule, SharedModule],
 })
 export class AppModule {}
