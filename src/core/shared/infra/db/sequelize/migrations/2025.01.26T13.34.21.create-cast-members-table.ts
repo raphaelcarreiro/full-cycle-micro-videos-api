c31326e991aab11f1beecc3c31c181a2be22c9f7
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2025-01-26 13:40:46 -0300

    feat: add migration to create cast member table

---
+++ src/core/shared/infra/db/sequelize/migrations/2025.01.26T13.34.21.create-cast-members-table.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import type { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable('cast_members', {
+    cast_member_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE(3),
+      allowNull: false,
+    },
+  });
+};
+export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable('categories');
+};
